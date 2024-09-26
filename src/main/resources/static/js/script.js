f23a6ff8d3785983820da1ef8ba7f33664f60291
dsklimenko <kdima2001@yandex.ru>
2024-09-26 20:39:05 +0300

    26.09.2024 | add histogram to homepage

--- src/main/resources/static/js/script.js
+++
@@ -1,9 +0,0 @@
-function showBookDetails(id) {
-    window.location.href = "/book?id=" + id;
-}
-
-function showSearchResult() {
-    var inputField = document.getElementById("keyword");
-    var inputValue = inputField.value
-    window.location.href = "/search?keyword=" + inputValue;
-}
